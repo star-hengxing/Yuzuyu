e107ce373614d604dcc47a25057c6c028c1493bb
star-hengxing <hengxings783@gmail.com>
2023-03-14 17:29:07 +0800

    feet: windows thread sleep

--- src/runtime/os/os.cpp
+++ src/runtime/os/os.cpp
@@ -1,3 +1,9 @@
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <ctime>
+#endif
+
 #include "os.hpp"
 
 NAMESPACE_BEGIN(os)
@@ -7,6 +13,16 @@ auto time() noexcept -> fast_io::unix_timestamp
     // using namespace std::chrono;
     // return duration_cast<nanoseconds>(system_clock::now().time_since_epoch()).count();
     return fast_io::posix_clock_gettime(fast_io::posix_clock_id::realtime);
+}
+
+auto sleep(usize milliseconds) noexcept -> void
+{
+#ifdef _WIN32
+    Sleep(milliseconds);
+#else
+    static_assert(false, "unimplemented!");
+    // TODO
+#endif
 }
 
 NAMESPACE_END(os)
--- src/runtime/os/os.hpp
+++ src/runtime/os/os.hpp
@@ -2,10 +2,12 @@
 
 #include <fast_io.h>
 
-#include <runtime/helper/macro.hpp>
+#include <runtime/helper/basic.hpp>
 
 NAMESPACE_BEGIN(os)
 // @note get unix timestamp
 auto time() noexcept -> fast_io::unix_timestamp;
+// @note sleep current thread
+auto sleep(usize milliseconds) noexcept -> void;
 
 NAMESPACE_END(os)
