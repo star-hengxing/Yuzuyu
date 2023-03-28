2e340b6f9569e2e03b62fdf398636abbfd2836df
star-hengxing <hengxings783@gmail.com>
2023-03-28 19:48:00 +0800

    feat: add callback for audio

--- src/runtime/os/audio/xaudio2.cpp
+++ src/runtime/os/audio/xaudio2.cpp
@@ -1,16 +1,29 @@
+#include <thread>
+
+#include <runtime/helper/debug.hpp>
+#include <runtime/helper/range.hpp>
 #include "Xaudio2.hpp"
 
 NAMESPACE_BEGIN(os::audio::detail)
 
 auto Xaudio2::initialize() noexcept -> const char*
 {
+    if (is_run)
+    {
+        return "audio is already initialized";
+    }
+
     HRESULT result;
     CoInitializeEx(nullptr, COINIT_MULTITHREADED);
-    if constexpr (global::is_debug)
-        result = XAudio2Create(&xaduio, XAUDIO2_DEBUG_ENGINE);
-    else
-        result = XAudio2Create(&xaduio);
-
+#ifdef LIBRARY_DEBUG
+    result = XAudio2Create(&xaduio, XAUDIO2_DEBUG_ENGINE);
+    XAUDIO2_DEBUG_CONFIGURATION debug{};
+    debug.TraceMask = XAUDIO2_LOG_ERRORS | XAUDIO2_LOG_WARNINGS;
+    debug.BreakMask = XAUDIO2_LOG_ERRORS;
+    xaduio->SetDebugConfiguration(&debug);
+#else
+    result = XAudio2Create(&xaduio);
+#endif
     if (FAILED(result))
     {
         return "XAudio2Create failed";
@@ -22,17 +35,50 @@ auto Xaudio2::initialize() noexcept -> const char*
         return "CreateMasteringVoice failed";
     }
 
-    xaudio_buffer = {};
+    is_run = true;
+    is_play = false;
+    std::thread{&Self::worker, this}.detach();
     return nullptr;
+}
+
+auto Xaudio2::set_callback(void* userdata, audio_callback callback) noexcept -> Self&
+{
+    this->userdata = userdata;
+    this->callback = callback;
+    return *this;
 }
 
 auto Xaudio2::start() noexcept -> bool
 {
-    return SUCCEEDED(source->Start(0));
+    const auto result = SUCCEEDED(source->Start(0));
+    if (!result)
+    {
+        return false;
+    }
+
+    is_play = true;
+    condition.notify_one();
+    return true;
 }
 
 auto Xaudio2::end() noexcept -> bool
 {
+    // set XAUDIO2_VOICE_STATE SamplesPlayed zero
+    // https://stackoverflow.com/questions/65754955/how-to-reset-a-ixaudio2sourcevoices-samplesplayed-counter-after-flushing-sour
+    // auto buffer = XAUDIO2_BUFFER{};
+    // buffer.Flags = XAUDIO2_END_OF_STREAM;
+    // buffer.pAudioData = ring_buffer.get() + index * MAX_SINGLE_BUFFER_SIZE;
+    // buffer.AudioBytes = 1;
+
+    // but crash
+    // if (FAILED(source->SubmitSourceBuffer(&buffer)))
+    //     return false;
+
+    {
+        std::lock_guard lock{mutex};
+        is_play = false;
+    }
+    index = 0;
     return SUCCEEDED(source->Stop(0));
 }
 
@@ -43,42 +89,35 @@ auto Xaudio2::set_volume(f32 volume) noexcept -> bool
 
 auto Xaudio2::set_format(const Format& format) noexcept -> bool
 {
-    WAVEFORMATEX waveform;
-    waveform.wFormatTag = WAVE_FORMAT_PCM;
-    waveform.wBitsPerSample = format.sample_bit;
-    waveform.nChannels = format.channels;
-    waveform.nSamplesPerSec = format.sample_rate;
-    waveform.nBlockAlign = waveform.nChannels * (waveform.wBitsPerSample / 8);
-    waveform.nAvgBytesPerSec = waveform.nBlockAlign * waveform.nSamplesPerSec;
-    waveform.cbSize = 0;
+    if (source)
+    {
+        source->DestroyVoice();
+    }
 
-    return SUCCEEDED(xaduio->CreateSourceVoice(&source, &waveform));
+    auto wf = WAVEFORMATEX{};
+    wf.wFormatTag = WAVE_FORMAT_PCM;
+    wf.wBitsPerSample = format.sample_bit;
+    wf.nChannels = format.channels;
+    wf.nSamplesPerSec = format.sample_rate;
+    wf.nBlockAlign = wf.nChannels * (wf.wBitsPerSample / 8);
+    wf.nAvgBytesPerSec = wf.nBlockAlign * wf.nSamplesPerSec;
+
+    return SUCCEEDED(xaduio->CreateSourceVoice(&source, &wf));
 }
 
-auto Xaudio2::write(const char* str, usize size) noexcept -> bool
+auto Xaudio2::write(const u8* const data, usize size) noexcept -> bool
 {
-    if (size > XAUDIO2_MAX_BUFFER_BYTES)
+    if (!is_play || size > XAUDIO2_MAX_BUFFER_BYTES) [[unlikely]]
         return false;
 
-    auto buffer = xaudio_buffer;
-    buffer.Flags = XAUDIO2_END_OF_STREAM;
-    buffer.pAudioData = reinterpret_cast<const BYTE*>(str);
-    buffer.AudioBytes = size;
-    if (FAILED(source->SubmitSourceBuffer(&buffer)))
-        return false;
-
-    buffer_size = size;
-    return true;
-}
-
-auto Xaudio2::get_remaining_size() noexcept -> usize
-{
-    XAUDIO2_VOICE_STATE state;
     source->GetState(&state);
-    if (state.BuffersQueued == 0)
-        return 0;
+    if (state.BuffersQueued >= XAUDIO2_MAX_QUEUED_BUFFERS) [[unlikely]]
+        return false;
 
-    return buffer_size - state.SamplesPlayed;
+    auto buffer = XAUDIO2_BUFFER{};
+    buffer.pAudioData = data;
+    buffer.AudioBytes = size;
+    return SUCCEEDED(source->SubmitSourceBuffer(&buffer));
 }
 
 Xaudio2::~Xaudio2()
@@ -88,12 +127,55 @@ Xaudio2::~Xaudio2()
         source->DestroyVoice();
         source = nullptr;
     }
+
     if (xaduio)
     {
         xaduio->Release();
         xaduio = nullptr;
     }
+
     CoUninitialize();
+    // join worker thread
+    is_run = false;
+    is_play = true;
+    condition.notify_one();
+}
+
+auto Xaudio2::worker() noexcept -> void
+{
+    while (true)
+    {
+        {
+            auto lock = std::unique_lock{mutex};
+            condition.wait(lock, [&] { return is_play; });
+        }
+
+        if (!is_run) [[unlikely]]
+            return;
+
+        source->GetState(&state);
+        do
+        {
+            if (state.BuffersQueued > MIN_BUFFER_SIZE_CALLBACK || !callback || !userdata)
+                break;
+
+            for ([[maybe_unused]] auto _ : range(MAX_BUFFER_SIZE - state.BuffersQueued))
+            {
+                const auto current_buffer = ring_buffer.get() + index * MAX_SINGLE_BUFFER_SIZE;
+                index = (index + 1) % MAX_BUFFER_SIZE;
+                std::invoke(callback, userdata, current_buffer, MAX_SINGLE_BUFFER_SIZE);
+
+                auto buffer = XAUDIO2_BUFFER{};
+                buffer.pAudioData = current_buffer;
+                buffer.AudioBytes = MAX_SINGLE_BUFFER_SIZE;
+                const auto result = SUCCEEDED(source->SubmitSourceBuffer(&buffer));
+                Assert(result);
+            }
+
+        } while (false);
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP_TIME));
+    }
 }
 
 NAMESPACE_END(os::audio::detail)
--- src/runtime/os/audio/xaudio2.hpp
+++ src/runtime/os/audio/xaudio2.hpp
@@ -1,43 +1,63 @@
 #pragma once
 
-#ifdef USE_MODULES
-import <array>;
-#else
-#include <array>
-#endif
-
+#include <condition_variable>
+#include <mutex>
 #include <XAudio2.h>
 
+#include <runtime/helper/Owned.hpp>
 #include "base.hpp"
 
 NAMESPACE_BEGIN(os::audio::detail)
 
+static constexpr auto MAX_SINGLE_BUFFER_SIZE = usize{4096};
+static constexpr auto MAX_BUFFER_SIZE = usize{4};
+static constexpr auto MIN_BUFFER_SIZE_CALLBACK = MAX_BUFFER_SIZE / 2;
+// unit: milliseconds
+static constexpr auto SLEEP_TIME = 10;
+
 struct Xaudio2
 {
+public:
+    using Self = Xaudio2;
+    using audio_callback = void (*)(void*, u8*, usize) noexcept;
+
 private:
     IXAudio2* xaduio = nullptr;
     IXAudio2MasteringVoice* master = nullptr;
     IXAudio2SourceVoice* source = nullptr;
 
-    usize buffer_size;
-    XAUDIO2_BUFFER xaudio_buffer;
+    XAUDIO2_VOICE_STATE state;
+
+    u8 index = 0;
+    // copy data to our buffer, keep the lifetime until the audio is played
+    Owned<u8[]> ring_buffer = Owned<u8[]>::make(MAX_SINGLE_BUFFER_SIZE * MAX_BUFFER_SIZE);
+
+    bool is_run = false;
+    bool is_play = false;
+    void* userdata = nullptr;
+    audio_callback callback = nullptr;
+    std::mutex mutex;
+    std::condition_variable condition;
+
+private:
+    auto worker() noexcept -> void;
 
 public:
     ~Xaudio2();
 
     auto initialize() noexcept -> const char*;
 
-    auto set_volume(f32 size) noexcept -> bool;
+    auto set_volume(f32 volume) noexcept -> bool;
 
     auto set_format(const Format& format) noexcept -> bool;
+
+    auto set_callback(void* userdata, audio_callback callback) noexcept -> Self&;
 
     auto start() noexcept -> bool;
 
     auto end() noexcept -> bool;
-
-    auto write(const char* str, usize size) noexcept -> bool;
-    // @note Get the audio remaining bytes in the current buffer.
-    auto get_remaining_size() noexcept -> usize;
+    // @note: get data ownership, keep the lifetime until the audio is played
+    auto write(const u8* const data, usize size) noexcept -> bool;
 };
 
 NAMESPACE_END(os::audio::detail)
